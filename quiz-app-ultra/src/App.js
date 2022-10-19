769b0784e00672aa6a21330c3f0625d51c4392d1
Julian Hirt <julian.hirt94@web.de>
2022-10-19 20:38:51 +0200

    add js and css components

--- quiz-app-ultra/src/App.js
+++ quiz-app-ultra/src/App.js
@@ -1,8 +1,26 @@
-import "./App.css";
+import "./global.css";
 import Header from "./components/Header/";
+import Card from "./components/Card";
+import Navbar from "./components/Navbar/";
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <main>
+        <ul className="card-list">
+          <Card
+            question="example Question"
+            answer="example answer"
+            tag="#tag"
+          />
+        </ul>
+      </main>
+      <footer>
+        <Navbar />
+      </footer>
+    </div>
+  );
 }
 
 export default App;
