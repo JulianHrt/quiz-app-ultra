84edd55a12840cd9a3c433847c6c1a07e888c6a3
Julian Hirt <julian.hirt94@web.de>
2022-10-20 12:16:32 +0200

    correct syntaxfailure

--- quiz-app-ultra/src/components/Header/index.js
+++ quiz-app-ultra/src/components/Header/index.js
@@ -2,8 +2,8 @@ import "./Header.css";
 
 function Header() {
   return (
-    <header class="header">
-      <h1 class="header__title">Quiz-App</h1>
+    <header className="header">
+      <h1 className="header__title">Quiz-App</h1>
     </header>
   );
 }
