f254590f37e08728135a4db1a9ec91bbada3fa1d
Julian Hirt <julian.hirt94@web.de>
2022-10-21 19:06:27 +0200

    add cards via inputfunction

--- quiz-app-ultra/src/components/utils/Array.js
+++ quiz-app-ultra/src/components/utils/Array.js
@@ -1,4 +1,4 @@
-export const cards = [
+export const myCardArray = [
   { id: 1, question: "Frage?", answer: "Antwort!", tags: [1, 2, 3] },
   { id: 2, question: "Frage2?", answer: "Antwort2!", tags: [1, 2, 3] },
   { id: 3, question: "Frage3?", answer: "Antwort3!", tags: [1, 2, 3] },
--- quiz-app-ultra/src/pages/AddPage.js
+++ quiz-app-ultra/src/pages/AddPage.js
@@ -1,3 +1,37 @@
-export default function AddPage() {
-  return <h1>addPage is currently under construction</h1>;
+import "./addPage.css";
+
+export default function AddPage({ appendCard, setActive }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log(data.answer, data.question, data.tags);
+
+    appendCard(data.question, data.answer, data.tags);
+
+    setActive("homePage");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label for="question">
+        Question
+        <textarea id="question" name="question"></textarea>
+      </label>
+      <label for="answer">
+        Answer
+        <textarea id="answer" name="answer"></textarea>
+      </label>
+      <label for="tags">
+        Tags
+        <textarea id="tags" name="tags"></textarea>
+      </label>
+      <label for="submit">
+        <button type="submit" id="submit">
+          submit
+        </button>
+      </label>
+    </form>
+  );
 }
